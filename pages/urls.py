d7e61dcb86192a1a0f02945f877d82a655938886
Fiona-T <fitracey@gmail.com>
2021-11-16 12:05:58 +0000

    feat: add initial structure in base.html, create home page view, wire up urls, create index.html

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+"""URL paths for the 'pages' app (static pages)"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePage.as_view(), name='home')
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
+"""Views for the 'pages' app (static pages)"""
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+
+class HomePage(generic.TemplateView):
+    """Home Page view - static page, no model"""
+    template_name = 'index.html'
