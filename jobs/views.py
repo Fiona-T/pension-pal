679ba9764fb51303b11451bbcc653bf569f19a07
Fiona-T <fitracey@gmail.com>
2021-11-25 17:57:36 +0000

    feat: update MyJobs view in jobs app to return list of job objects added by that user

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,14 @@
 """Views for the 'jobs' app (create, view, edit, delete jobs)"""
 from django.shortcuts import render
 from django.views import generic
-# Create your views here.
+from .models import Job
 
 
-class MyJobs(generic.TemplateView):
-    """My Jobs page view """
+class MyJobs(generic.ListView):
+    """My Jobs page view, displays list of jobs added by user"""
+    model = Job
     template_name = 'my-jobs.html'
+
+    def get_queryset(self):
+        """filter the objects by the current user"""
+        return Job.objects.filter(added_by=self.request.user)
