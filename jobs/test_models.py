d57e0ad004516db3c50af9790f83f02af1fd630d
Fiona-T <fitracey@gmail.com>
2021-11-25 15:48:42 +0000

    test: create tests for Job model in jobs app,test_models.py, run tests

---
+++ jobs/test_models.py
@@ -0,0 +1,45 @@
+"""Unit tests for Models for 'jobs' app"""
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Job
+
+
+class TestModels(TestCase):
+    """To test the model for 'jobs' app"""
+
+    def setUp(self):
+        """
+        Create a user for tests, user needed to create records from Job model
+        """
+        self.user = User.objects.create(
+            username='fred',
+            password='secret',
+        )
+
+    def test_full_or_part_time_defaults_to_full(self):
+        """
+        Create a record from Job model without setting the
+        full_or_part_time field, check this field was set to full
+        """
+        job = Job.objects.create(
+            added_by=self.user,
+            employer_name='Test employer',
+            start_date='2020-01-01',
+            finish_date='2020-12-01',
+            )
+        self.assertEqual(job.full_or_part_time, 0)
+
+    def test_job_string_method_returns_employer_name(self):
+        """
+        Check that employer name created in test record is returned
+        by the string method called on that record
+        """
+        job = Job.objects.create(
+            added_by=self.user,
+            employer_name='Test Company',
+            start_date='2020-01-01',
+            finish_date='2020-12-01',
+            full_or_part_time=1,
+            )
+
+        self.assertEqual(str(job), 'Test Company')
