4e3b987a58fd6d5d5f956111d2e99583d0c3aed4
Fiona-T <fitracey@gmail.com>
2021-11-26 16:50:24 +0000

    feat: add LoginRequiredMixin to MyJobs view, to redirect if user not logged in

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,16 @@
 """Views for the 'jobs' app (create, view, edit, delete jobs)"""
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Job
 
 
-class MyJobs(generic.ListView):
-    """My Jobs page view, displays list of jobs added by user"""
+class MyJobs(LoginRequiredMixin, generic.ListView):
+    """
+    My Jobs page view, displays list of jobs added by user
+    If user not logged in, redirects to login page, then to jobs once signed in
+    handled by the LoginRequiredMixin
+    """
     model = Job
     template_name = 'my-jobs.html'
 
