89a1a62a17c383896148ff612a4f2c31f27319b8
Fiona-T <fitracey@gmail.com>
2021-11-29 17:35:51 +0000

    test: update text_views in jobs app with AddJobView tests and amend url in MyJobs test

--- jobs/test_views.py
+++ jobs/test_views.py
@@ -35,8 +35,8 @@ class TestMyJobsListView(TestCase):
 
     def test_redirects_if_not_logged_in(self):
         """ Test that user is redirected to correct page if not logged in """
-        response = self.client.get('/jobs')
-        self.assertRedirects(response, '/accounts/login/?next=/jobs')
+        response = self.client.get('/jobs/')
+        self.assertRedirects(response, '/accounts/login/?next=/jobs/')
 
     def test_correct_url_and_template_for_logged_in_user(self):
         """
@@ -45,7 +45,7 @@ class TestMyJobsListView(TestCase):
         and check the correct template is used
         """
         self.client.login(username='fred', password='secret1234')
-        response = self.client.get('/jobs')
+        response = self.client.get('/jobs/')
         self.assertEqual(str(response.context['user']), 'fred')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'my-jobs.html')
@@ -57,8 +57,36 @@ class TestMyJobsListView(TestCase):
         and the jobs displayed are those with added_by equal to user
         """
         self.client.login(username='jane', password='secret1234567')
-        response = self.client.get('/jobs')
+        response = self.client.get('/jobs/')
         self.assertEqual(str(response.context['user']), 'jane')
         self.assertTrue('job_list' in response.context)
         for job in response.context['job_list']:
             self.assertEqual(response.context['user'], job.added_by)
+
+
+class TestAddJobView(TestCase):
+    """ To test the AddJob view """
+    def setUp(self):
+        """ Create a test user """
+        test_user = User.objects.create_user(
+            username='fred',
+            password='secret1234',
+        )
+        test_user.save()
+
+    def test_redirects_if_not_logged_in(self):
+        """ Test that user is redirected to correct page if not logged in """
+        response = self.client.get('/jobs/add/')
+        self.assertRedirects(response, '/accounts/login/?next=/jobs/add/')
+
+    def test_correct_url_and_template_for_logged_in_user(self):
+        """
+        login the test user, get the url for add-job page
+        check the user is logged in, that the response is 200 (i.e. successful)
+        and check the correct template is used
+        """
+        self.client.login(username='fred', password='secret1234')
+        response = self.client.get('/jobs/add/')
+        self.assertEqual(str(response.context['user']), 'fred')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add-job.html')
