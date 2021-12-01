c45e368568b09928cbb86d76976492e2995d6dab
Fiona-T <fitracey@gmail.com>
2021-12-01 16:39:37 +0000

    test: add test for AddJobSuccess view, update AddJob view with new redirect page, jobs.test_views.py

--- jobs/test_views.py
+++ jobs/test_views.py
@@ -108,6 +108,34 @@ class TestAddJobView(TestCase):
             'finish_date': '2020-12-01',
             'full_or_part_time': 1,
         })
-        self.assertRedirects(response, '/jobs/')
+        self.assertRedirects(response, '/jobs/success/')
         response = self.client.get('/jobs/')
         self.assertEqual(len(response.context['job_list']), 1)
+
+
+class TestAddJobSuccessView(TestCase):
+    """ To test the AddJobSuccess view """
+    def setUp(self):
+        """ Create a test user """
+        test_user = User.objects.create_user(
+            username='fred',
+            password='secret1234',
+        )
+        test_user.save()
+
+    def test_redirects_if_not_logged_in(self):
+        """ Test that user is redirected to correct page if not logged in """
+        response = self.client.get('/jobs/success/')
+        self.assertRedirects(response, '/accounts/login/')
+
+    def test_correct_url_and_template_for_logged_in_user(self):
+        """
+        login the test user, get the url for add-job-success page
+        check the user is logged in, that the response is 200 (i.e. successful)
+        and check the correct template is used
+        """
+        self.client.login(username='fred', password='secret1234')
+        response = self.client.get('/jobs/success/')
+        self.assertEqual(str(response.context['user']), 'fred')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add-job-success.html')
