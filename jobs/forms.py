2c86b562498a17cc090e0e15cfca757a8a14d7b8
Fiona-T <fitracey@gmail.com>
2021-12-02 12:29:00 +0000

    feat: update AddJobForm to show date format to user and include datepicker, jobs.forms.py

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,16 +1,27 @@
 """Forms for the 'jobs' app - add job and edit job """
-from django.forms import ModelForm
+from django import forms
 from .models import Job
 
 
-class AddJobForm(ModelForm):
+class AddJobForm(forms.ModelForm):
     """ Form for user to add a job from, Add Job button from My Jobs page """
     class Meta:
-        """ Form uses the Job model """
+        """
+        Based on Job model, all fields except added by.Helptext for finish date
+        Widget for date fields to show format to user, include the date picker
+        """
         model = Job
         fields = [
             'employer_name', 'start_date', 'finish_date', 'full_or_part_time'
             ]
         help_texts = {
             'finish_date': ('Finish date should be after the start date.')
+            }
+        widgets = {
+            'start_date': forms.DateInput(
+                format=('%d/%m/%Y'),
+                attrs={'class': 'form-control', 'type': 'date'}),
+            'finish_date': forms.DateInput(
+                format=('%d/%m/%Y'),
+                attrs={'class': 'form-control', 'type': 'date'}),
             }
