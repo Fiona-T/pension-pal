e6b0e8545e940723748da556d5685cb7074b8da8
Fiona-T <fitracey@gmail.com>
2021-12-03 16:11:41 +0000

    chore: change setUp to setUpTestData for class level set up, jobs.test_models.py

--- jobs/test_models.py
+++ jobs/test_models.py
@@ -6,15 +6,16 @@ from .models import Job
 
 class TestModels(TestCase):
     """To test the model for 'jobs' app"""
-
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         """
         Create a user for tests, user needed to create records from Job model
         """
-        self.user = User.objects.create(
+        test_user = User.objects.create_user(
             username='fred',
-            password='secret',
+            password='secret1234',
         )
+        test_user.save()
 
     def test_full_or_part_time_defaults_to_full(self):
         """
@@ -22,7 +23,7 @@ class TestModels(TestCase):
         full_or_part_time field, check this field was set to full
         """
         job = Job.objects.create(
-            added_by=self.user,
+            added_by=User.objects.get(username='fred'),
             employer_name='Test employer',
             start_date='2020-01-01',
             finish_date='2020-12-01',
@@ -35,11 +36,10 @@ class TestModels(TestCase):
         by the string method called on that record
         """
         job = Job.objects.create(
-            added_by=self.user,
+            added_by=User.objects.get(username='fred'),
             employer_name='Test Company',
             start_date='2020-01-01',
             finish_date='2020-12-01',
             full_or_part_time=1,
             )
-
         self.assertEqual(str(job), 'Test Company')
