80563197c0ea3ed3748d40e38fe3204d6b465aa8
Fiona-T <fitracey@gmail.com>
2021-12-07 17:23:29 +0000

    feat: add url for deleting a job, from delete job modal to urls.py, jobs app

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', views.AddJob.as_view(), name='add_job'),
     path('success/', views.AddJobSuccess.as_view(), name='add_job_success'),
     path('edit/<job_id>', views.EditJob.as_view(), name='edit_job'),
+    path('delete/<job_id>', views.DeleteJob.as_view(), name='delete_job')
 ]
