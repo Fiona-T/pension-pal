39dac56b30271af9aad75ff8c4981e1b29a85516
Fiona-T <fitracey@gmail.com>
2021-12-09 17:32:10 +0000

    build: create Provider model in pensions app, make and run migrations, register in admin

--- pensions/admin.py
+++ pensions/admin.py
@@ -1,3 +1,5 @@
+"""Register the models for the 'pensions' app for admin panel"""
 from django.contrib import admin
+from .models import Provider
 
-# Register your models here.
+admin.site.register(Provider)
--- pensions/models.py
+++ pensions/models.py
@@ -1,3 +1,18 @@
+"""Models for the 'pensions' app (create, view, edit, delete pensions)"""
 from django.db import models
 
-# Create your models here.
+STATUS = ((0, 'Active'), (1, 'Not active'))
+
+
+class Provider(models.Model):
+    """Model for pension provider details, records added in admin panel"""
+    provider_name = models.CharField(max_length=50, unique=True)
+    website = models.URLField(max_length=50)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        """order the records by name of provider"""
+        ordering = ['provider_name']
+
+    def __str__(self):
+        return self.provider_name
