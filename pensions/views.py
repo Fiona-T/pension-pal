e50541d9bcccb9ab6d35fc0bf098d3c6cb547c2b
Fiona-T <fitracey@gmail.com>
2021-12-15 18:46:31 +0000

    feat: update the AddPension view to display PensionForm for get request

--- pensions/views.py
+++ pensions/views.py
@@ -1,6 +1,8 @@
 """Views for the pensions app"""
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PensionForm
 
 
 class MyPensions(generic.TemplateView):
@@ -8,6 +10,14 @@ class MyPensions(generic.TemplateView):
     template_name = 'my-pensions.html'
 
 
-class AddPension(generic.TemplateView):
-    """Add pension view"""
-    template_name = 'add-pension.html'
+class AddPension(LoginRequiredMixin, View):
+    """Add pension view - display form on get request, post form on post req"""
+    def get(self, request):
+        """Display the form"""
+        return render(
+            request,
+            'add-pension.html',
+            {
+                "form": PensionForm()
+            }
+        )
