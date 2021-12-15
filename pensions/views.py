cb5b299fa320e317ae6515f6d06f7823a9c51b3b
Fiona-T <fitracey@gmail.com>
2021-12-15 14:55:18 +0000

    feat: create basic AddPension template view to display the add-pension page

--- pensions/views.py
+++ pensions/views.py
@@ -6,3 +6,8 @@ from django.views import generic
 class MyPensions(generic.TemplateView):
     """My Pensions page view"""
     template_name = 'my-pensions.html'
+
+
+class AddPension(generic.TemplateView):
+    """Add pension view"""
+    template_name = 'add-pension.html'
