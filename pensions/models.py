75b38cff4d97273f3f88d240c700f28e9c4e82c8
Fiona-T <fitracey@gmail.com>
2021-12-15 14:41:37 +0000

    build: create Pension model in pensions.models.py and run migrations

--- pensions/models.py
+++ pensions/models.py
@@ -1,7 +1,16 @@
 """Models for the 'pensions' app (create, view, edit, delete pensions)"""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from jobs.models import Job
 
 STATUS = ((0, 'Active'), (1, 'Not active'))
+PENSION_TYPE = (
+    (0, 'Occupational Pension Scheme'),
+    (1, 'PRSA'),
+    (2, 'RAC / Personal Pension'),
+    (3, 'Retirement Bond'),
+    )
 
 
 class Provider(models.Model):
@@ -16,3 +25,31 @@ class Provider(models.Model):
 
     def __str__(self):
         return self.provider_name
+
+
+class Pension(models.Model):
+    """Model for Pension records added by user via form on frontend"""
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='pensions')
+    employment = models.ForeignKey(
+        Job, on_delete=models.CASCADE, related_name='linked_pensions')
+    scheme_name = models.CharField(max_length=100)
+    policy_number = models.CharField(max_length=15)
+    member_number = models.CharField(max_length=15, blank=True)
+    pension_type = models.IntegerField(choices=PENSION_TYPE, default=0)
+    date_joined_scheme = models.DateField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    pao = models.BooleanField(default=False)
+    director = models.BooleanField(default=False)
+    pension_provider = models.ForeignKey(
+        Provider, on_delete=models.CASCADE, related_name='recorded_pensions')
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    file = CloudinaryField(blank=True)
+    notes = models.TextField(blank=True)
+
+    class Meta:
+        """order by date joined the scheme"""
+        ordering = ['-date_joined_scheme']
+
+    def __str__(self):
+        return f'{self.scheme_name} for {self.added_by}'
