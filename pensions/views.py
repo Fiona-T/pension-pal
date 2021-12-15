f3985260d6b0a58cc788ad1a45e15aba9eb7d043
Fiona-T <fitracey@gmail.com>
2021-12-15 21:40:57 +0000

    feat: update MyPensions view to retrieve users pensions to display on my-pensions template

--- pensions/views.py
+++ pensions/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import PensionForm
+from .models import Pension
 
 
-class MyPensions(generic.TemplateView):
+class MyPensions(LoginRequiredMixin, generic.ListView):
     """My Pensions page view"""
+    model = Pension
     template_name = 'my-pensions.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        """filter the objects by the current user"""
+        return Pension.objects.filter(added_by=self.request.user)
 
 
 class AddPension(LoginRequiredMixin, View):
