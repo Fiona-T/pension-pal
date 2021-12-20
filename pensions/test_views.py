beca1eb09dce78d8a4a37ffdf452a0def3a1a9ad
Fiona-T <fitracey@gmail.com>
2021-12-20 16:22:46 +0000

    test: create and run tests for EditPensionView, pensions.test_views.py

--- pensions/test_views.py
+++ pensions/test_views.py
@@ -197,3 +197,134 @@ class TestAddPensionView(TestCase):
         self.assertRedirects(response, '/pensions/success/')
         response = self.client.get('/pensions/')
         self.assertEqual(len(response.context['pension_list']), 1)
+
+
+class TestEditPensionView(TestCase):
+    """Tests for the EditPension view"""
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create two test users.
+        Create provider and 2 job records, needed as foreign keys in Pension
+        Create a pension record. All records created by test_user1.
+        """
+        test_user1 = User.objects.create_user(
+            username='Tester',
+            password='topsecret1234',
+            )
+        test_user2 = User.objects.create_user(
+            username='jane',
+            password='secret1234567',
+            )
+        test_user1.save()
+        test_user2.save()
+
+        Job.objects.create(
+            added_by=User.objects.get(username='Tester'),
+            employer_name='Test Company',
+            start_date='2020-01-01',
+            finish_date='2020-12-01',
+            full_or_part_time=1,
+            )
+
+        Job.objects.create(
+            added_by=User.objects.get(username='Tester'),
+            employer_name='Second Test Company',
+            start_date='2004-04-15',
+            finish_date='2020-12-01',
+            full_or_part_time=0,
+            )
+
+        Provider.objects.create(
+            provider_name='A Pension Provider',
+            website='wwww.awebsite.ie',
+            status=1,
+            )
+
+        Pension.objects.create(
+            added_by=test_user1,
+            employment=Job.objects.get(id=1),
+            scheme_name='Test pension scheme',
+            policy_number='12345',
+            pension_type=1,
+            date_joined_scheme='2020-01-01',
+            salary=10000,
+            pao=True,
+            director=True,
+            pension_provider=Provider.objects.get(id=1),
+            value=1000,
+            )
+
+    def test_redirects_if_not_logged_in(self):
+        """User is redirected to correct page if not logged in"""
+        response = self.client.get('/pensions/edit/1')
+        self.assertRedirects(
+            response, '/accounts/login/?next=/pensions/edit/1'
+            )
+
+    def test_correct_url_and_template_for_logged_in_user_own_pensions(self):
+        """
+        Login the test user, get the url for edit-pension page, using id of 1
+        (this will be the id of the pension added in set up)
+        check the user is logged in, that the response is 200 (i.e. successful)
+        and check the correct template is used
+        """
+        self.client.login(username='Tester', password='topsecret1234')
+        response = self.client.get('/pensions/edit/1')
+        self.assertEqual(str(response.context['user']), 'Tester')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit-pension.html')
+
+    def test_can_edit_own_pension_detail(self):
+        """
+        Login test user, go to edit url, post to edit url with update to some
+        fields. Check redirects to my-pensions page after form submitted.
+        Get the pension record, check the fields have the updated details.
+        """
+        self.client.login(username='Tester', password='topsecret1234')
+        response = self.client.get('/pensions/edit/1')
+        response = self.client.post('/pensions/edit/1', {
+            'added_by': 'Tester',
+            'employment': str(2),
+            'scheme_name': "New Pension Scheme Name",
+            'policy_number': '12345',
+            'pension_type': 1,
+            'date_joined_scheme': '2020-01-01',
+            'salary': 50000,
+            'pao': False,
+            'director': False,
+            'pension_provider': str(1),
+            'value': 36789,
+            })
+        self.assertRedirects(response, '/pensions/')
+        updated_pension = Pension.objects.get(id=1)
+        self.assertEqual(
+            updated_pension.scheme_name, 'New Pension Scheme Name'
+            )
+        self.assertEqual(
+            str(updated_pension.employment), 'Second Test Company'
+            )
+        self.assertEqual(updated_pension.salary, 50000)
+        self.assertEqual(updated_pension.pao, False)
+        self.assertEqual(updated_pension.director, False)
+        self.assertEqual(updated_pension.value, 36789)
+
+    def test_404_returned_logged_in_but_invalid_pension_id(self):
+        """
+        Logged in user gets 404 response trying to access edit page using
+        an invalid pension id, i.e one which does not exist
+        """
+        self.client.login(username='Tester', password='topsecret1234')
+        response = self.client.get('/pensions/edit/5')
+        self.assertEqual(response.status_code, 404)
+
+    def test_cannot_access_edit_page_for_other_user_pension(self):
+        """
+        Logged in user who did not create the pension gets 404 response when
+        trying to access the edit page with pension id not created by them
+        """
+        self.client.login(username='jane', password='secret1234567')
+        response = self.client.get('/pensions/')
+        self.assertEqual(str(response.context['user']), 'jane')
+        response = self.client.get('/pensions/edit/1')
+        self.assertEqual(response.status_code, 404)
