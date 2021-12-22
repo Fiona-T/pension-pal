482dd1e9a55896280c10c8020d83c3ee10509980
Fiona-T <fitracey@gmail.com>
2021-12-22 17:41:06 +0000

    build: register Pension model in admin.py

--- pensions/admin.py
+++ pensions/admin.py
@@ -1,5 +1,7 @@
 """Register the models for the 'pensions' app for admin panel"""
 from django.contrib import admin
-from .models import Provider
+from .models import Provider, Pension
 
 admin.site.register(Provider)
+
+admin.site.register(Pension)
