3533c74b4030a1621f25db41ea8a81e2cf3fb6f9
Fiona-T <fitracey@gmail.com>
2022-01-12 19:20:39 +0000

    feat: add custom error msg for unique constraint on employer name on Job form, jobs.forms.py

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,6 @@
 """Forms for the 'jobs' app - add job and edit job """
 from django import forms
+from django.core.exceptions import NON_FIELD_ERRORS
 from .models import Job
 
 
@@ -10,6 +11,7 @@ class AddJobForm(forms.ModelForm):
         Based on Job model, all fields except added by.Helptext for finish date
         Widget for date fields to show format to user & include the date picker
         Date format set to yyyy-mm-dd, format displayed to user is dd/mm/yyyy
+        Custom error message for unique constraint on employer_name per user.
         """
         model = Job
         fields = [
@@ -26,3 +28,9 @@ class AddJobForm(forms.ModelForm):
                 format=('%Y-%m-%d'),
                 attrs={'type': 'date'}),
             }
+        error_messages = {
+            NON_FIELD_ERRORS: {
+                'unique_together': 'You already have a Job with this Employer'
+                ' name. Choose a different name for this new Job record.'
+            }
+        }
