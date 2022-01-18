85701054e564cdd5af5b1a9b43013ed8b45f85de
Fiona-T <fitracey@gmail.com>
2022-01-18 15:16:37 +0000

    feat: add ProviderAdmin model in pensions.admin.py with list fields, filter and search

--- pensions/admin.py
+++ pensions/admin.py
@@ -2,6 +2,17 @@
 from django.contrib import admin
 from .models import Provider, Pension
 
-admin.site.register(Provider)
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    """
+    Provider model - admin set up:
+    Show all fields in list display, allow filtering by status,
+    Allow search by provider name or website.
+    """
+    list_display = ('provider_name', 'website', 'status')
+    list_filter = ('status', )
+    search_fields = ['provider_name', 'website']
+
 
 admin.site.register(Pension)
