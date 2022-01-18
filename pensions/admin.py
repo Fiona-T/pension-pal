c9c4ac77a69e288d55007a8975f5e79431e65e6e
Fiona-T <fitracey@gmail.com>
2022-01-18 15:21:49 +0000

    feat: update ProviderAdmin to allow editing by website or status in list view, pensions.admin.py

--- pensions/admin.py
+++ pensions/admin.py
@@ -9,9 +9,11 @@ class ProviderAdmin(admin.ModelAdmin):
     Provider model - admin set up:
     Show all fields in list display, allow filtering by status,
     Allow search by provider name or website.
+    Allow editing of website and status via list view.
     """
     list_display = ('provider_name', 'website', 'status')
     list_filter = ('status', )
+    list_editable = ('website', 'status')
     search_fields = ['provider_name', 'website']
 
 
