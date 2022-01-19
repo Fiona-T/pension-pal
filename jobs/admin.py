5ea039b36ea2830971e70970dc443e26654b9e89
Fiona-T <fitracey@gmail.com>
2022-01-19 11:47:09 +0000

    feat: set up admin site for Jobs model - list display, filtering, search, jobs.admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,9 +3,22 @@ from django.contrib import admin
 from .models import Job
 
 
+@admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    """Admin panel set up for Job model - show the id field"""
+    """
+    Job model - admin set up:
+    Show all fields in list display, allow filtering by user who added record,
+    Allow search by employer name. Employer name is a link to view the record.
+    Id field shown as readonly field for information purposes.
+    """
     readonly_fields = ('id',)
-
-
-admin.site.register(Job, JobAdmin)
+    list_display = (
+        'added_by',
+        'employer_name',
+        'start_date',
+        'finish_date',
+        'full_or_part_time'
+        )
+    list_filter = ('added_by',)
+    list_display_links = ('employer_name',)
+    search_fields = ['employer_name']
